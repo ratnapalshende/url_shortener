761dabd285554fe7bc12052bc16643c7736a90af
ratnapalshende <ratnapalshende2001@gmail.com>
2025-02-02 07:28:25 +0530

    fix bug

--- build.sh
+++ build.sh
@@ -8,5 +8,6 @@ pip install -r requirements.txt
 # Convert static asset files
 python manage.py collectstatic --no-input
 
+python manage.py makemigrations
 # Apply any outstanding database migrations
 python manage.py migrate
